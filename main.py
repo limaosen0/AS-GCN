3041264b149fc4b541c586277e263430088b29c7
Maosen Li <limaosen_uestc@163.com>
2019-06-16 23:46:48 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import argparse
+import sys
+import torchlight
+from torchlight import import_class
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Processor collection')
+
+    processors = dict()
+    processors['recognition'] = import_class('processor.recognition.REC_Processor')
+    processors['demo'] = import_class('processor.demo.Demo')
+
+    subparsers = parser.add_subparsers(dest='processor')
+    for k, p in processors.items():
+        subparsers.add_parser(k, parents=[p.get_parser()])
+
+    arg = parser.parse_args()
+
+    # start
+    Processor = processors[arg.processor]
+    p = Processor(sys.argv[2:])
+    p.start()

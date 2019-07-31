b44dc3ee0cacc25e1db4d575268ce26733bdc3fa
Maosen Li <limaosen_uestc@163.com>
2019-07-31 09:18:45 +0800

    Add files via upload

---
+++ data_gen/preprocess.py
@@ -0,0 +1,68 @@
+import sys
+
+sys.path.extend(['../'])
+from data_gen.rotation import *
+from tqdm import tqdm
+
+
+def pre_normalization(data, zaxis=[0, 1], xaxis=[8, 4]):
+    N, C, T, V, M = data.shape
+    s = np.transpose(data, [0, 4, 2, 3, 1])
+
+    print('sub the center joint')
+    for i_s, skeleton in enumerate(tqdm(s)):
+        if skeleton.sum() == 0:
+            continue
+        main_body_center = skeleton[0][:, 1:2, :].copy()
+        for i_p, person in enumerate(skeleton):
+            if person.sum() == 0:
+                continue
+            mask = (person.sum(-1) != 0).reshape(T, V, 1)
+            s[i_s, i_p] = (s[i_s, i_p] - main_body_center) * mask
+
+
+    print('parallel the torso bone')
+    for i_s, skeleton in enumerate(tqdm(s)):
+        if skeleton.sum() == 0:
+            continue
+        joint_bottom = skeleton[0, 0, zaxis[0]]
+        joint_top = skeleton[0, 0, zaxis[1]]
+        axis = np.cross(joint_top - joint_bottom, [0, 0, 1])
+        angle = angle_between(joint_top - joint_bottom, [0, 0, 1])
+        matrix_z = rotation_matrix(axis, angle)
+        for i_p, person in enumerate(skeleton):
+            if person.sum() == 0:
+                continue
+            for i_f, frame in enumerate(person):
+                if frame.sum() == 0:
+                    continue
+                for i_j, joint in enumerate(frame):
+                    s[i_s, i_p, i_f, i_j] = np.dot(matrix_z, joint)
+
+
+    print('parallel the shoulder bone')
+    for i_s, skeleton in enumerate(tqdm(s)):
+        if skeleton.sum() == 0:
+            continue
+        joint_rshoulder = skeleton[0, 0, xaxis[0]]
+        joint_lshoulder = skeleton[0, 0, xaxis[1]]
+        axis = np.cross(joint_rshoulder - joint_lshoulder, [1, 0, 0])
+        angle = angle_between(joint_rshoulder - joint_lshoulder, [1, 0, 0])
+        matrix_x = rotation_matrix(axis, angle)
+        for i_p, person in enumerate(skeleton):
+            if person.sum() == 0:
+                continue
+            for i_f, frame in enumerate(person):
+                if frame.sum() == 0:
+                    continue
+                for i_j, joint in enumerate(frame):
+                    s[i_s, i_p, i_f, i_j] = np.dot(matrix_x, joint)
+
+    data = np.transpose(s, [0, 4, 2, 3, 1])
+    return data
+
+
+if __name__ == '__main__':
+    data = np.load('../data/NTU-RGB+D/xsub/train_data.npy')
+    pre_normalization(data)
+    np.save('../data/nturgb_d/xsub/data_train_pre.npy', data)
